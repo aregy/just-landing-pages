99e1be096c178dc5b5f4039a57984fa798aa133e
Areg Yeghnazar <areg@are.gy>
2021-11-18 23:50:32 -0800

    Added areg.xyz stub

---
+++ aregxyz/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>areg.xyz</title>
+    <link rel="stylesheet" href="/Content/css/bootstrap.css" />
+    <link rel="stylesheet" href="/Content/css/site.css" />
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+  </head>
+  <body>
+    <div class="container">
+        <main role="main" class="pb-3">
+
+<div class="row" style="text-align: center;">
+    <div>
+    </div>
+    <div style="font-size: 45px; color: #5599AA;  letter-spacing: 6px; color:#335566;">areg.xyz</div>
+    </footer>
+</div>
+
+        </main>
+    </div>
+
+    <!--<footer class="border-top footer text-muted">
+        <div class="container">
+            &copy; 2020 - G2DCore-->
+    <!--</div>
+    </footer>
+    <script src="/lib/jquery/dist/jquery.min.js"></script>
+    <script src="/lib/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/js/site.js?v=9HKvIUtTd5dRXZ3G3m4fjhwztTJRUotJCN2Lr9tcbpQ"></script>-->
+
+    </body>
+  </html>
